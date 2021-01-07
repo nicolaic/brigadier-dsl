276782d10af36f8def6e8d338563c5c61a172e64
Nicolai <contact@nicolai.dev>
2021-01-07 03:41:01 +0100

    Add DSL command implementation and underlying node structure

---
+++ src/main/kotlin/dev/nicolai/brigadier/dsl/DslCommand.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2020-present Nicolai Christophersen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.nicolai.brigadier.dsl
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder
+import com.mojang.brigadier.context.CommandContext
+import dev.nicolai.brigadier.Command
+import dev.nicolai.brigadier.RequiredArgument
+
+class DslCommand<S>(
+    private val literal: String,
+    private val builderBlock: (DslCommandBuilder<S>.() -> Unit)
+) : Command<S> {
+
+    override fun buildLiteral(): LiteralArgumentBuilder<S> {
+        val dslNode = LiteralDslCommandNode<S>(literal, ContextRef())
+
+        val builder = DslCommandBuilder(dslNode)
+        builder.apply(builderBlock)
+
+        return dslNode.buildTree()
+    }
+}
+
+class DslCommandBuilder<S> internal constructor(private val dslNode: DslCommandNode<S, *>) {
+
+    fun executes(command: (CommandContext<S>) -> Int) {
+        dslNode.executes(command)
+    }
+
+    infix fun runs(command: (S) -> Unit) {
+        dslNode.runs(command)
+    }
+
+    fun literal(literal: String, block: (DslCommandBuilder<S>.() -> Unit)? = null): DslCommandBuilder<S> {
+        val literalNode = dslNode.literal(literal)
+
+        val builder = DslCommandBuilder(literalNode)
+        block?.invoke(builder)
+
+        return builder
+    }
+
+    fun literals(vararg literals: String, block: (DslCommandBuilder<S>.() -> Unit)? = null): DslCommandBuilder<S> {
+        val literalNode = literals.fold(dslNode) { node, literal -> node.literal(literal) }
+
+        val builder = DslCommandBuilder(literalNode)
+        block?.invoke(builder)
+
+        return builder
+    }
+
+    fun <T : Any> arg(arg: RequiredArgument<S, T>, block: DslCommandBuilder<S>.(() -> T) -> Unit) {
+        val argNode = dslNode.argument(arg)
+
+        val builder = DslCommandBuilder(argNode)
+        block(builder, argNode.getter)
+    }
+}
+
+fun <S> command(literal: String, block: DslCommandBuilder<S>.() -> Unit) = DslCommand(literal, block)
---
+++ src/main/kotlin/dev/nicolai/brigadier/dsl/DslCommandNode.kt
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2020-present Nicolai Christophersen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.nicolai.brigadier.dsl
+
+import com.mojang.brigadier.Command
+import com.mojang.brigadier.builder.ArgumentBuilder
+import com.mojang.brigadier.builder.LiteralArgumentBuilder
+import com.mojang.brigadier.builder.RequiredArgumentBuilder
+import com.mojang.brigadier.context.CommandContext
+import dev.nicolai.brigadier.CommandArgument
+
+sealed class DslCommandNode<S, A : ArgumentBuilder<S, out A>>(
+    private val contextRef: ContextRef<S>
+) {
+    private var executes: ((CommandContext<S>) -> Int)? = null
+
+    private val children = mutableListOf<DslCommandNode<S, out ArgumentBuilder<S, *>>>()
+
+    fun executes(command: (CommandContext<S>) -> Int) {
+        executes = command
+    }
+
+    fun runs(command: (S) -> Unit) {
+        executes { context ->
+            command(context.source)
+            Command.SINGLE_SUCCESS
+        }
+    }
+
+    fun literal(literal: String): LiteralDslCommandNode<S> {
+        return LiteralDslCommandNode(literal, contextRef).also { children += it}
+    }
+
+    fun <T> argument(argument: CommandArgument<S, T>): ArgumentDslCommandNode<S, T> {
+        return ArgumentDslCommandNode(argument, contextRef).also { children += it}
+    }
+
+    abstract fun buildNode(): A
+
+    fun buildTree(): A {
+        val node = buildNode()
+
+        executes?.let { executes ->
+            node.executes { context ->
+                contextRef.context = context
+                executes.invoke(context)
+            }
+        }
+
+        children
+            .map { it.buildTree() }
+            .forEach { node.then(it) }
+
+        return node
+    }
+}
+
+class LiteralDslCommandNode<S>(
+    private val literal: String,
+    contextRef: ContextRef<S>
+) : DslCommandNode<S, LiteralArgumentBuilder<S>>(contextRef) {
+    override fun buildNode() = LiteralArgumentBuilder.literal<S>(literal)
+}
+
+class ArgumentDslCommandNode<S, T>(
+    private val argument: CommandArgument<S, T>,
+    contextRef: ContextRef<S>
+) : DslCommandNode<S, RequiredArgumentBuilder<S, out T>>(contextRef) {
+
+    val getter: () -> T = { argument.getValue(contextRef.context) }
+
+    override fun buildNode() = argument.buildArgument()
+}
+
+class ContextRef<S> {
+    lateinit var context: CommandContext<S>
+}
