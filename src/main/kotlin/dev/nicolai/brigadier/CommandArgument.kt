30ab356468379004c8556b9efb5463cc44b9f916
Nicolai <contact@nicolai.dev>
2021-01-20 10:25:24 +0100

    Add getter default function for optional arguments

--- src/main/kotlin/dev/nicolai/brigadier/CommandArgument.kt
+++ src/main/kotlin/dev/nicolai/brigadier/CommandArgument.kt
@@ -20,7 +20,7 @@ import com.mojang.brigadier.arguments.ArgumentType
 import com.mojang.brigadier.builder.RequiredArgumentBuilder
 import com.mojang.brigadier.context.CommandContext
 
-sealed class CommandArgument<S, T, out V> {
+sealed class CommandArgument<S, T, V> {
 
     abstract fun getValue(context: CommandContext<S>): V
 
@@ -40,29 +40,26 @@ class RequiredArgument<S, T>(
     override fun buildArgument(): RequiredArgumentBuilder<S, T> =
         RequiredArgumentBuilder.argument<S, T>(name, type).also { apply?.invoke(it) }
 
-    override fun apply(block: RequiredArgumentBuilder<S, T>.() -> Unit): RequiredArgument<S, T> {
-        this.apply = block
-        return this
-    }
+    override fun apply(block: RequiredArgumentBuilder<S, T>.() -> Unit) =
+        this.also { this.apply = block }
 }
 
 class OptionalArgument<S, T : D, D>(
     private val argument: RequiredArgument<S, T>,
-    private val default: D
+    private val default: (S) -> D
 ) : CommandArgument<S, T, D>() {
 
     override fun getValue(context: CommandContext<S>) = try {
         argument.getValue(context)
     } catch (e: IllegalArgumentException) {
-        default
+        default(context.source)
     }
 
     override fun buildArgument() = argument.buildArgument()
-    override fun apply(block: RequiredArgumentBuilder<S, T>.() -> Unit): OptionalArgument<S, T, D> {
-        argument.apply(block)
-        return this
-    }
+    override fun apply(block: RequiredArgumentBuilder<S, T>.() -> Unit) =
+        this.also { argument.apply(block) }
 }
 
-fun <S, T> RequiredArgument<S, T>.optional() = OptionalArgument(this, null)
-fun <S, T> RequiredArgument<S, T>.default(value: T) = OptionalArgument(this, value)
+fun <S, T> RequiredArgument<S, T>.optional() = OptionalArgument(this) { null }
+fun <S, T> RequiredArgument<S, T>.default(value: T) = OptionalArgument(this) { value }
+fun <S, T> RequiredArgument<S, T>.defaultGetter(value: (S) -> T) = OptionalArgument(this, value)
